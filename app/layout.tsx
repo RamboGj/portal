9142f97252e112df6116ed5352fe516f53fd201b
RamboGj <jpramboguanabara@hotmail.com>
2025-04-15 21:49:43 -0300

    remvoe theming

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,6 @@ import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import "./globals.css";
-import { cookies } from "next/headers";
-
 
 const openSans = Open_Sans({
   variable: "--font-open-sans",
@@ -28,12 +26,11 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const cookiesStore = await cookies()
-
-  const theme = cookiesStore.get("theme")?.value || "dark"
+  // const cookiesStore = await cookies()
+  // const theme = cookiesStore.get("theme")?.value || "dark"
 
   return (
-    <html className={theme} lang="en">
+    <html lang="en">
       <body
         className={`${openSans.variable} font-Satoshi antialiased`}
       >
